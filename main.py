fb001c5a25b454685e57a377a0db4583de9270a7
zui0202 <65958007+zui0202@users.noreply.github.com>
2023-03-09 18:30:58 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    for i in range(0,len(nums)):
+        for j in range(i+1,len(nums)):
+            sum=nums[i]+nums[j]
+            if(sum==target):
+                return [i,j]
